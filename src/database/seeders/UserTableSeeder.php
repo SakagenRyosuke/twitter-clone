0b08a902c824623f521f1067bf0c5326e82a9fdd
SakagenRyosuke <ryousuke.sakagen@gmail.com>
2022-06-17 16:31:37 +0900

    ユーザーシードの追加

---
+++ src/database/seeders/UserTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 30; $i++) {
+            User::create([
+                'name'    => 'test_user' .$i,
+                'screenName'           => 'TEST' .$i,
+                'profileImage'  => 'https://placehold.jp/50x50.png',
+                'email'          => 'test' .$i .'@test.com',
+                'password'       => Hash::make('password'),
+                'remember_token' => Str::random(10),
+                'created_at'     => now(),
+                'updated_at'     => now()
+            ]);
+        }
+    }
+}
