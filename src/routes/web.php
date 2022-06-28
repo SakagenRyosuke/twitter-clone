67e1daba1216301fd91af0b3ddd6993bec043b1f
SakagenRyosuke <ryousuke.sakagen@gmail.com>
2022-06-28 15:29:38 +0900

    ルーティングにコメント追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,25 +18,74 @@ use App\Http\Controllers\FollowController;
 
 Auth::routes();
 
-Route::get('/loginUser', [UserController::class, 'loginUser']);
-Route::get('/maxPage', [UserController::class, 'maxPage']);
-Route::post('/logout', [UserController::class, 'logout']);
-Route::get('/userList/{page}', [UserController::class, 'userList']);
-Route::get('/userProfile/{id}', [UserController::class, 'show']);
-Route::get('/userProfile/{id}/following', [FollowController::class, 'getNumFollowing']);
-Route::get('/userProfile/{id}/followed', [FollowController::class, 'getNumFollowed']);
-Route::get('/users/followList', [FollowController::class, 'followList']);
-Route::post('/users/{id}/follow', [FollowController::class, 'follow']);
-Route::post('/users/{id}/unfollow', [FollowController::class, 'unfollow']);
-
 Route::group(['middleware' => 'auth'], function () {
+  /**
+   * /にアクセスした場合welcome.bladeを表示する
+   * welcome.blade.php
+   */
   Route::get('/', function () {
     return view('welcome');
   });
-
+  /**
+   * /homeにアクセスした場合home.bladeを表示する
+   * home.blade.php
+   */
   Route::get('/home', [HomeController::class, 'index'])->name('home');
-
+  /**
+   * /home以下にアクセスした場合home.bladeを表示する
+   * home.blade.php
+   */
   Route::get('/home/{any}', function () {
     return view('home');
   })->where('any', '.*');
+  /**
+   * ログインユーザーの取得
+   * NavComponent
+   */
+  Route::get('/loginUser', [UserController::class, 'getLoginUser']);
+  /**
+   * ログアウトの実行
+   * NavComponent
+   */
+  Route::post('/logout', [UserController::class, 'logout']);
+  /**
+   * 最大ページ数の取得
+   * UserListComponent
+   */
+  Route::get('/maxPage', [UserController::class, 'getMaxPage']);
+  /**
+   * ユーザー一覧の取得
+   * UserListComponent
+   */
+  Route::get('/userList/{page}', [UserController::class, 'getUserList']);
+  /**
+   * idと一致するユーザーの取得
+   * UserListDetailComponent
+   */
+  Route::get('/userProfile/{id}', [UserController::class, 'show']);
+  /**
+   * idと一致するユーザーのフォローしている人数の取得
+   * UserListDetailComponent
+   */
+  Route::get('/userProfile/{id}/followingCount', [FollowController::class, 'getFollowingCount']);
+  /**
+   * idと一致するユーザーのフォローされている人数の取得
+   * UserListDetailComponent
+   */
+  Route::get('/userProfile/{id}/followedCount', [FollowController::class, 'getFollowedCount']);
+  /**
+   * ログインユーザーのフォローリストの取得
+   * FollowButtonComponent
+   */
+  Route::get('/users/followList', [FollowController::class, 'getFollowList']);
+  /**
+   * フォローの実行
+   * FollowButtonComponent
+   */
+  Route::post('/users/{id}/follow', [FollowController::class, 'follow']);
+  /**
+   * フォロー解除の実行
+   * FollowButtonComponent
+   */
+  Route::post('/users/{id}/unfollow', [FollowController::class, 'unfollow']);
 });
