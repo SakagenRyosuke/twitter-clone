1807c271b0a250ec9003111180d3adf665ca2162
SakagenRyosuke <ryousuke.sakagen@gmail.com>
2022-07-01 17:33:49 +0900

    tweetテーブルの作成、シーダーの作成

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserTableSeeder::class,
             FollowTableSeeder::class,
+            TweetTableSeeder::class,
         ]);
     }
 }
