529355c5657054f6f72beaabcd8cb56143e32868
SakagenRyosuke <ryousuke.sakagen@gmail.com>
2022-07-11 04:14:57 +0900

    paginateの数の変更

--- src/app/Consts/Paginate.php
+++ src/app/Consts/Paginate.php
@@ -5,6 +5,6 @@ namespace App\Consts;
 
 class Paginate
 {
-    public const NUM_TWEET_PER_PAGE = 10;
-    public const NUM_USER_PER_PAGE = 10;
+    public const NUM_TWEET_PER_PAGE = 15;
+    public const NUM_USER_PER_PAGE = 15;
 }
